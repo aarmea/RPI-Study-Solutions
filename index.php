e944f26535692f4409be864580d5f2dc3514fdd7
Alex <alexkumbar@gmail.com>
2013-11-13 17:37:21 -0500

    Added divs

--- index.php
+++ index.php
@@ -4,5 +4,8 @@
   <title>RPI Study Solutions</title>
 </head>
 <body>
+  <div id="topBarDiv">this will have navigation links</div>
+  <div id="signUpDiv"></div>
+  <div id="aboutDiv"></div>
 </body>
 </html>
