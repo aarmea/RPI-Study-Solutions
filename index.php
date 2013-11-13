973715f065664ab7ddca4fcce38285e7be6d06c8
Alex <alexkumbar@gmail.com>
2013-11-13 17:47:13 -0500

    Updates on index

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
 </head>
 <body>
   <div id="topBarDiv">this will have navigation links</div>
-  <div id="signUpDiv"></div>
-  <div id="aboutDiv"></div>
+  <div id="signUpDiv"><a href="google.com">button goes here</a></div>
+  <div id="aboutDiv">
+    <h2>Project Summary</h2>
+    <p>Real, Practical, and Informative Study Solutions, or RPI Study Solutions, is a website application that will benefit RPI and its students and professors. Students will be able to search for and form study groups. RPI will be able to more efficiently use underutilized classrooms. RPI professors will have more motivated students. We will host tools to help them set up meeting times and locations. Students will need to login to their accounts in order to form or be accepted into a group. We will also create a forum/moderator tool to help the groups make decisions about projects and share ideas. This will be the main functionality of the website, but we have many ideas to add to it depending on how much we can get done within our time limit. We believe that this website will help college students who desire to study in groups or need to organize group projects.</p>
+</div>
 </body>
 </html>
