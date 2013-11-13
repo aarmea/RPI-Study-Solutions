8d2dd567fd441c90148359a446ae9af0656c8238
Alex <alexkumbar@gmail.com>
2013-11-13 18:12:57 -0500

    Index page with place holder content

--- index.php
+++ index.php
@@ -5,11 +5,20 @@
   <link rel="stylesheet" type="text/css" href="resources/style.css">
 </head>
 <body>
-  <div id="topBarDiv">this will have navigation links</div>
-  <div id="signUpDiv"><a href="google.com">button goes here</a></div>
+  <div id="topBarDiv">
+    <p>this will have navigation links</p>
+  </div>
+  <div id="content">
+  <div id="signUpDiv">
+    <a href="google.com"><img src="resources/images/login-button-blue-hi.png" class="loginImage"></a>
+  </div>
   <div id="aboutDiv">
     <h2>Project Summary</h2>
     <p>Real, Practical, and Informative Study Solutions, or RPI Study Solutions, is a website application that will benefit RPI and its students and professors. Students will be able to search for and form study groups. RPI will be able to more efficiently use underutilized classrooms. RPI professors will have more motivated students. We will host tools to help them set up meeting times and locations. Students will need to login to their accounts in order to form or be accepted into a group. We will also create a forum/moderator tool to help the groups make decisions about projects and share ideas. This will be the main functionality of the website, but we have many ideas to add to it depending on how much we can get done within our time limit. We believe that this website will help college students who desire to study in groups or need to organize group projects.</p>
-</div>
+  </div>
+  </div>
+  <div id="footer">
+    <p>footer!</p>
+  </div>
 </body>
 </html>
