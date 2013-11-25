32a73ac52f08935fdbc44260981925f5b9393737
Albert Armea <albert@albertarmea.com>
2013-11-24 23:56:19 -0500

    Add a logout page

--- public/auth/login.php
+++ public/auth/login.php
@@ -3,13 +3,8 @@
 // https://wiki.jasig.org/display/CASC/phpCAS+installation+guide
 require_once "CAS.php";
 
-require_once "config.php";
+require_once "cas_init.php";
 
-// TODO: Switch this off for production
-phpCAS::setDebug();
-
-phpCAS::client(CAS_VERSION_2_0, $CAS_HOST, $CAS_PORT, $CAS_URI);
-phpCAS::setCasServerCACert($CAS_CA_CERT_PATH);
 phpCAS::forceAuthentication();
 
 ?>
