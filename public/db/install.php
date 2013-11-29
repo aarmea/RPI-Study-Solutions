ffc761e5f9c2f6ef822057fb339607873a1065e2
Albert Armea <albert@albertarmea.com>
2013-11-29 02:28:54 -0500

    Rename database directory

--- public/db/install.php
+++ public/db/install.php
@@ -1,31 +1,30 @@
 <?php
-$host="localhost"; 
+$host="localhost";
 
 require "config.php";
 
-$root= $config['DB_USERNAME']; 
-$root_password= $config['DB_PASSWORD']; 
+$root= $config['DB_USERNAME'];
+$root_password= $config['DB_PASSWORD'];
 
 $user='newuser';
 $pass='newpass';
-$db="websysfinaldatabase"; 
+$db="websysfinaldatabase";
 
-    try {
-        $dbh = new PDO("mysql:host=$host", $root, $root_password);
+try {
+  $dbh = new PDO("mysql:host=$host", $root, $root_password);
 
-        $dbh->exec("CREATE DATABASE if not exists `$db`;
-                CREATE USER '$user'@'localhost' IDENTIFIED BY '$pass';
-                GRANT ALL ON `$db`.* TO '$user'@'localhost';
-                FLUSH PRIVILEGES;") 
-        or die(print_r($dbh->errorInfo(), true));
-
-    } catch (PDOException $e) {
-        die("DB ERROR: ". $e->getMessage());
-    }
+  $dbh->exec("CREATE DATABASE if not exists `$db`;
+    CREATE USER '$user'@'localhost' IDENTIFIED BY '$pass';
+    GRANT ALL ON `$db`.* TO '$user'@'localhost';
+    FLUSH PRIVILEGES;")
+  or die(print_r($dbh->errorInfo(), true));
+} catch (PDOException $e) {
+  die("DB ERROR: ". $e->getMessage());
+}
 
 $sql = "USE websysfinaldatabase";
 $dbh->exec($sql);
- 
+
 $sql = "CREATE TABLE if not exists tutor (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `rcs` varchar(200) NOT NULL,
@@ -33,12 +32,8 @@ $sql = "CREATE TABLE if not exists tutor (
   `lname` varchar(100) NOT NULL,
   `email` varchar(250) NOT NULL,
   PRIMARY KEY (`id`)
-)";
-
-
- $dbh->exec($sql);
-
-
+  )";
+$dbh->exec($sql);
 
 $sql = "CREATE TABLE IF NOT EXISTS `user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -47,8 +42,7 @@ $sql = "CREATE TABLE IF NOT EXISTS `user` (
   `lname` varchar(100) NOT NULL,
   `email` varchar(250) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB";
-
+  ) ENGINE=InnoDB";
 $dbh->exec($sql);
 
 $sql = "CREATE TABLE IF NOT EXISTS `groups` (
@@ -58,9 +52,9 @@ $sql = "CREATE TABLE IF NOT EXISTS `groups` (
   PRIMARY KEY (`g_id`),
   KEY `owner_id` (`owner_id`),
   KEY `g_id` (`g_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2";
- $dbh->exec($sql);
-  
+  ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2";
+$dbh->exec($sql);
+
 $sql = "CREATE TABLE IF NOT EXISTS `posts` (
   `p_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `t_id` int(10) unsigned NOT NULL,
@@ -71,7 +65,7 @@ $sql = "CREATE TABLE IF NOT EXISTS `posts` (
   PRIMARY KEY (`p_id`),
   KEY `t_id` (`t_id`),
   KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ";
+  ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ";
 $dbh->exec($sql);
 
 $sql = "CREATE TABLE IF NOT EXISTS `threads` (
@@ -80,21 +74,21 @@ $sql = "CREATE TABLE IF NOT EXISTS `threads` (
   `threadName` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`t_id`),
   KEY `group_id` (`group_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ";
+  ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ";
 $dbh->exec($sql);
 
 $sql = "ALTER TABLE `groups`
-  ADD CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `user` (`id`)";
+ADD CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `user` (`id`)";
 $dbh->exec($sql);
 
 $sql = "ALTER TABLE `posts`
-  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`t_id`) REFERENCES `threads` (`t_id`),
-  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)";
+ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`t_id`) REFERENCES `threads` (`t_id`),
+ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)";
 $dbh->exec($sql);
 
 $sql = "ALTER TABLE `threads`
-  ADD CONSTRAINT `threads_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`g_id`)";
+ADD CONSTRAINT `threads_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`g_id`)";
 $dbh->exec($sql);
 
- echo "Database created";
+echo "Database created";
 ?>
