f1416f31145113abf9567027dc3652658dff77fc
Albert Armea <albert@albertarmea.com>
2013-12-01 12:13:37 -0500

    Move user creation functions to user.php

--- public/classes/user.php
+++ public/classes/user.php
@@ -2,6 +2,7 @@
 require_once "auth/config.php";
 require_once "db/init.php";
 
+$DIRECTORY_API_URL = "http://rpidirectory.appspot.com/api";
 $IMAGE_API_URL = "http://www.gravatar.com/avatar";
 
 class User {
@@ -74,5 +75,58 @@ class User {
     $hash = md5(strtolower(trim($email)));
     return "$IMAGE_API_URL/$hash?s=$size&d=identicon";
   }
+}
+
+function getYogFromClass($class) {
+  $currentDate = new DateTime;
+  $yog = (int)$currentDate->format("Y");
+  if ((int)$currentDate->format("m") > 5) {
+    $yog += 1;
+  }
+  switch($class) {
+  case "first-year student":
+    $yog += 3;
+    break;
+  case "sophomore":
+    $yog += 2;
+    break;
+  case "junior":
+    $yog += 1;
+    break;
+  case "senior":
+    break;
+  }
+  return $yog;
+}
+
+function addUserFromDirectory($rcsid) {
+  global $db; // PDO defined in "db/init.php"
+  global $DIRECTORY_API_URL;
+
+  // Request from the RPI Directory app
+  // See https://github.com/jewishdan18/RPI-Directory/wiki/API-Documentation
+  $directoryResponse = json_decode(
+    file_get_contents("$DIRECTORY_API_URL?q=$rcsid"));
+
+  $query = $db->prepare(
+    "INSERT INTO `users` (`rcsid`, `shortname`, `fullname`, `yog`, `major`)
+    VALUES (:rcsid, :shortname, :fullname, :yog, :major)"
+  );
+  foreach ($directoryResponse->data as $user) {
+    // Only accept an exact match
+    if ($user->rcsid != $rcsid) continue;
+
+    $query->execute(array(
+      ":rcsid" => $user->rcsid,
+      ":shortname" => ucwords($user->first_name),
+      ":fullname" => $user->name,
+      ":yog" => getYogFromClass($user->year),
+      ":major" => ucwords($user->major)
+    ));
+
+    break;
+  }
+
+  return new User($rcsid);
 }
 ?>
