9d09675ec6c8f7150e8684b34b554d92476f76d4
Albert Armea <albert@albertarmea.com>
2013-11-30 22:58:49 -0500

    Add groups and group membership SQL tables

--- public/db/install.php
+++ public/db/install.php
@@ -4,6 +4,7 @@ echo "<html><pre>";
 
 try {
   $dbh = new PDO("mysql:host=$DB_HOST", $DB_USERNAME, $DB_PASSWORD);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Successfully connected to the database server\n";
 
   $dbh->exec("CREATE DATABASE if not exists `$DB_NAME`
@@ -14,68 +15,79 @@ try {
   die("DB ERROR: ". $e->getMessage());
 }
 
-$sql = "USE $DB_NAME";
-$dbh->exec($sql);
-echo "Successfully created database $DB_NAME.\n";
+try {
+  $sql = "USE $DB_NAME";
+  $dbh->exec($sql);
+  echo "Successfully created database $DB_NAME.\n";
 
-$sql = "CREATE TABLE IF NOT EXISTS `users` (
-  `rcsid` VARCHAR(10) NOT NULL,
-  `shortname` VARCHAR(64) NOT NULL,
-  `fullname` VARCHAR(64) NOT NULL,
-  `email` VARCHAR(256),
-  `yog` YEAR,
-  `major` VARCHAR(64),
-  PRIMARY KEY (`rcsid`)
-  ) ENGINE=InnoDB";
-$dbh->exec($sql);
-echo "Successfully created the users table.\n";
+  $sql = "CREATE TABLE IF NOT EXISTS `users` (
+    `rcsid` VARCHAR(10) NOT NULL,
+    `shortname` VARCHAR(64) NOT NULL,
+    `fullname` VARCHAR(64) NOT NULL,
+    `email` VARCHAR(256),
+    `yog` YEAR,
+    `major` VARCHAR(64),
+    PRIMARY KEY (`rcsid`)
+    ) ENGINE=InnoDB";
+  $dbh->exec($sql);
+  echo "Successfully created the users table.\n";
 
-/*
-$sql = "CREATE TABLE IF NOT EXISTS `groups` (
-  `g_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `groupName` varchar(255) NOT NULL,
-  `owner_id` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`g_id`),
-  KEY `owner_id` (`owner_id`),
-  KEY `g_id` (`g_id`)
-  ) ENGINE=InnoDB";
-$dbh->exec($sql);
+  $sql = "CREATE TABLE IF NOT EXISTS `groups` (
+    `groupid` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `groupname` varchar(255) NOT NULL,
+    PRIMARY KEY (`groupid`)
+    ) ENGINE=InnoDB";
+  $dbh->exec($sql);
+  echo "Successfully created the groups table.\n";
 
-$sql = "CREATE TABLE IF NOT EXISTS `posts` (
-  `p_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `t_id` int(10) unsigned NOT NULL,
-  `user_id` int(10) unsigned NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `postBody` varchar(3000) NOT NULL,
-  `postNumInThread` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`p_id`),
-  KEY `t_id` (`t_id`),
-  KEY `user_id` (`user_id`)
-  ) ENGINE=InnoDB";
-$dbh->exec($sql);
+  $sql = "CREATE TABLE IF NOT EXISTS `group_members` (
+    `groupid` INT UNSIGNED NOT NULL,
+    `rcsid` VARCHAR(10) NOT NULL,
+    `is_owner` BOOL DEFAULT FALSE,
+    PRIMARY KEY (`groupid`),
+    FOREIGN KEY (`groupid`) REFERENCES `groups`(`groupid`)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (`rcsid`) REFERENCES `users`(`rcsid`)
+    ON UPDATE CASCADE ON DELETE CASCADE
+    ) ENGINE=InnoDB";
+  $dbh->exec($sql);
+  echo "Successfully created the group membership table.\n";
 
-$sql = "CREATE TABLE IF NOT EXISTS `threads` (
-  `t_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `group_id` int(10) unsigned NOT NULL,
-  `threadName` varchar(255) NOT NULL,
-  PRIMARY KEY (`t_id`),
-  KEY `group_id` (`group_id`)
-  ) ENGINE=InnoDB";
-$dbh->exec($sql);
+  /*
+  $sql = "CREATE TABLE IF NOT EXISTS `posts` (
+    `p_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `t_id` int(10) unsigned NOT NULL,
+    `user_id` int(10) unsigned NOT NULL,
+    `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `postBody` varchar(3000) NOT NULL,
+    `postNumInThread` int(10) unsigned NOT NULL,
+    PRIMARY KEY (`p_id`),
+    KEY `t_id` (`t_id`),
+    KEY `user_id` (`user_id`)
+    ) ENGINE=InnoDB";
+  $dbh->exec($sql);
 
-$sql = "ALTER TABLE `groups`
-ADD CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `user` (`id`)";
-$dbh->exec($sql);
+  $sql = "CREATE TABLE IF NOT EXISTS `threads` (
+    `t_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `group_id` int(10) unsigned NOT NULL,
+    `threadName` varchar(255) NOT NULL,
+    PRIMARY KEY (`t_id`),
+    KEY `group_id` (`group_id`)
+    ) ENGINE=InnoDB";
+  $dbh->exec($sql);
 
-$sql = "ALTER TABLE `posts`
-ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`t_id`) REFERENCES `threads` (`t_id`),
-ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)";
-$dbh->exec($sql);
+  $sql = "ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`t_id`) REFERENCES `threads` (`t_id`),
+  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)";
+  $dbh->exec($sql);
 
-$sql = "ALTER TABLE `threads`
-ADD CONSTRAINT `threads_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`g_id`)";
-$dbh->exec($sql);
- */
+  $sql = "ALTER TABLE `threads`
+  ADD CONSTRAINT `threads_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`g_id`)";
+  $dbh->exec($sql);
+   */
+} catch (PDOException $e) {
+  exit("Database error:\n" . $e->getMessage());
+}
 
 echo "Installation succeeded.";
 ?>
