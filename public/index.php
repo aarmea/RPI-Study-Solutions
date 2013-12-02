888bfb3877937ba78471fdec370dae412ec0315f
Chris Parais <chris.j.paradis@gmail.com>
2013-12-01 22:05:20 -0500

    Changes to index.php

--- public/index.php
+++ public/index.php
@@ -1,11 +1,21 @@
+<!DOCTYPE html>
 <?php include "resources/head.php"; ?>
+<html>
+<link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
 <body>
   <?php include "resources/topbar.php"; ?>
   <div id="content">
   <div id="signUpDiv">
+<<<<<<< HEAD
     <a href="login.php"><img src="resources/images/login-button-blue-hi.png" class="loginImage"></a>
+=======
+    <img src="resources/images/mathgirl.jpg" class="loginImage">
+    <h2>Project Summary</h2>
+    <p>Real, Practical, and Informative Study Solutions, or RPI Study Solutions, is a website application that will benefit RPI and its students and professors. Students will be able to search for and form study groups. RPI will be able to more efficiently use underutilized classrooms. RPI professors will have more motivated students. We will host tools to help them set up meeting times and locations. Students will need to login to their accounts in order to form or be accepted into a group. We will also create a forum/moderator tool to help the groups make decisions about projects and share ideas. This will be the main functionality of the website, but we have many ideas to add to it depending on how much we can get done within our time limit. We believe that this website will help college students who desire to study in groups or need to organize group projects.</p>    
+>>>>>>> CSS Changes
   </div>
   <div id="aboutDiv">
+    <img src="resources/images/mathgirl.jpg" class="loginImage">
     <h2>Project Summary</h2>
     <p>Real, Practical, and Informative Study Solutions, or RPI Study Solutions, is a website application that will benefit RPI and its students and professors. Students will be able to search for and form study groups. RPI will be able to more efficiently use underutilized classrooms. RPI professors will have more motivated students. We will host tools to help them set up meeting times and locations. Students will need to login to their accounts in order to form or be accepted into a group. We will also create a forum/moderator tool to help the groups make decisions about projects and share ideas. This will be the main functionality of the website, but we have many ideas to add to it depending on how much we can get done within our time limit. We believe that this website will help college students who desire to study in groups or need to organize group projects.</p>
   </div>
