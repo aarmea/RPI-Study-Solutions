cde44fe2c8f2e6dab643f5a5f372a41ac4b32a87
Albert Armea <albert@albertarmea.com>
2013-12-02 11:16:49 -0500

    Revert "Re added threads and posts tables to install"
    This reverts commit ad6c204cc0e005e66b6a85eb8ed880727c572d3d.

--- public/db/install.php
+++ public/db/install.php
@@ -52,11 +52,11 @@ try {
   $dbh->exec($sql);
   echo "Successfully created the group membership table.\n";
 
-  
+  /*
   $sql = "CREATE TABLE IF NOT EXISTS `posts` (
     `p_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
     `t_id` int(10) unsigned NOT NULL,
-    `user_id` VARCHAR(10) NOT NULL,
+    `user_id` int(10) unsigned NOT NULL,
     `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `postBody` varchar(3000) NOT NULL,
     `postNumInThread` int(10) unsigned NOT NULL,
@@ -65,29 +65,25 @@ try {
     KEY `user_id` (`user_id`)
     ) ENGINE=InnoDB";
   $dbh->exec($sql);
-  echo "Successfully created the posts table.\n";
 
   $sql = "CREATE TABLE IF NOT EXISTS `threads` (
     `t_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-    `group_id` INT UNSIGNED NOT NULL,
+    `group_id` int(10) unsigned NOT NULL,
     `threadName` varchar(255) NOT NULL,
     PRIMARY KEY (`t_id`),
     KEY `group_id` (`group_id`)
     ) ENGINE=InnoDB";
   $dbh->exec($sql);
-  echo "Successfully created the threads table.\n";
 
   $sql = "ALTER TABLE `posts`
   ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`t_id`) REFERENCES `threads` (`t_id`),
-  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`rcsid`)";
+  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)";
   $dbh->exec($sql);
-  echo "Successfully linked posts.\n";
 
   $sql = "ALTER TABLE `threads`
-  ADD CONSTRAINT `threads_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`groupid`)";
+  ADD CONSTRAINT `threads_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`g_id`)";
   $dbh->exec($sql);
-  echo "Successfully linked threads.\n";
-   
+   */
 } catch (PDOException $e) {
   exit("Database error:\n" . $e->getMessage());
 }
