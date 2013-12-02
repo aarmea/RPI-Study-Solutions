cf58e84b2837641ca23c11aea16158aa18990886
Albert Armea <albert@albertarmea.com>
2013-12-02 11:16:34 -0500

    Revert "Fixed config files"
    This reverts commit e2a6e6be5412bcec4155a58610f4ff1821ca0afb.

--- public/db/config.php
+++ public/db/config.php
@@ -2,11 +2,10 @@
 // This configuration is valid only for the included Vagrant testing
 // environment. For production use, it is highly recommended to create a new
 // non-root MySQL user that only has access to $DB_NAME.
-$config = array(
-   'DB_HOST'     => 'localhost',
-   'DB_DBNAME'   => 'rpi_study_solutions',
-   'DB_USERNAME' => 'myadmin',
-   'DB_PASSWORD' => 'myadmin',
-);
+$DB_HOST = "localhost";
+$DB_NAME = "rpi_study_solutions";
+$DB_USERNAME = "myadmin";
+$DB_PASSWORD = "myadmin";
+
 date_default_timezone_set("America/New_York");
 ?>
