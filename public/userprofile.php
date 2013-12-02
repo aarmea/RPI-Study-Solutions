c2efd5c2014d1d974051deb2cbd8937a6a8e16ae
matizm <matiz.megan@gmail.com>
2013-12-02 13:19:28 -0500

    saves notes, still not posting saved notes

--- public/userprofile.php
+++ public/userprofile.php
@@ -1,13 +1,11 @@
 <?php
+require_once "db/init.php";
+require 'db/config.php';
 require_once "auth/cas_init.php";
 require_once "classes/user.php";
 
 phpCAS::forceAuthentication();
 $client = new User(phpCAS::getUser());
-
-// TODO: A way to show profiles specified with $_GET["u"]
-
-include "resources/head.php";
 ?>
 <!doctype html>
 <html>
@@ -27,12 +25,11 @@ include "resources/head.php";
     <p><?=$client->major()?>, Class of <?=$client->yog()?></p>
     <a href="settings.php">Settings</a>
     <div id="divOfGroups">
-      <h3>list of subscribed groups</h3>
-      <!-- TODO: Populate this list -->
+      <h3>Subscribed Groups:</h3>
       <ul id="ulOfGroups"></ul>
-      <!-- TODO: List of groups you administrate/own -->
+      <a href="createGroup.php">Create a group</a>
     </div>
-    <div id="">
+    <div id="meetings">
       <h3>Next Meeting:</h3>
       <h4><span id="groupName"></span> on <span id="date"></span> at <span id="time"></span></h4>
     </div>
@@ -59,16 +56,23 @@ include "resources/head.php";
     <div id="notes">
       <h3>Notes to self / Reminders</h3>
       <!-- post old content if it exists -->
-      <?php echo $var = isset($_POST['notesContent']) ? $_POST['notesContent'] : ''; ?>
+      <?php 
+      $res = $db->prepare("SELECT `notes` FROM `users` WHERE `rcsid` = :username");
+      $res->execute(array(':username'=>$client->username()));
+      $results=$res->fetch();
+      ?>
+      <div id="savedNotes"><p>Saved Notes:<?php echo $results; ?></p></div>
       <!-- create new content -->
-      <textarea id="notes" rows="10" cols="100" placeholder="My notes..."><textarea>
+      <form method="post" class="notes">
+      <textarea id="notes" name="notes" rows="10" cols="100" placeholder="My notes..."></textarea>
       <input type="submit" name="save" value="Save" action="userprofile.php"/>
+      </form>
       <!--save content -->
-      <?php if ($_POST['submit'])
+      <?php if ($_POST['save'])
       {
-        $text = mysql_real_escape_string($_POST['notes']); 
-        $query="INSERT INTO users (notes) VALUES ('$text')";
-        mysql_query($query) or die ('Error updating database' . mysql_error());
+        $query=$db->prepare("UPDATE `users` SET `notes`=:text WHERE `rcsid`= :username");
+        $query->execute(array(':username'=>$client->username(),
+                              ':text'=>$_POST['notes']));
       }?>
     </div>
   </div>
