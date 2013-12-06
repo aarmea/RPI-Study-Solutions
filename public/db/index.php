5353e98631b0fe5a51bcb8928b365f1b42202a58
Alex <alexkumbar@gmail.com>
2013-12-05 23:04:24 -0500

    updated db folder

--- public/db/index.php
+++ public/db/index.php
@@ -12,17 +12,8 @@
 <form action="install.php" method="get">
   <input type="submit" value="Install">
 </form>
-<form action="insert.php" method="get">
-  <input type="submit" value="Insert">
-</form>
-<form action="output_students.php" method="get">
-  <input type="submit" name ="Users" value="Users">
-</form>
-<form action="output_tutors.php" method="get">
-  <input type="submit" name ="Tutors" value="Tutors">
-</form>
-<form action="output_groups.php" method="get">
-  <input type="submit" name ="Groups" value="Groups">
+<form action="uninstall.php" method="get">
+  <input type="submit" value="Install">
 </form>
 </body>
 </html>
